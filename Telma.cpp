d55023105dcc1561638939b9771f638ff1f60f27
Tovo Faniry <randrianarivomananatovofaniry@gmail.com>
2026-01-20 16:00:14 +0300

    Fixation probleme << ; >> et test compilation

--- Telma.cpp
+++ Telma.cpp
@@ -26,30 +26,32 @@ int main(){
         }else if(choix == 4){
         
             cout << "Services YAS" << endl;
-            cout << "1 Info credit" << endl << "2 Recharge" << endl << "3 Gerer Friends and Family" << endl << "4 Envoyer Credit/Offre/Mega" << endl << "5 Rappelle moi" << endl << "6 Acheter une offre" << endl << "7 Ajouter des jours de validite" << endl << "8 Changement de Langue" << "9 Recuperer mon Numero" << "10 Mon numero" << "00 Page precedente" << endl << endl
+            cout << "1 Info credit" << endl << "2 Recharge" << endl << "3 Gerer Friends and Family" << endl << "4 Envoyer Credit/Offre/Mega" << endl << "5 Rappelle moi" << endl << "6 Acheter une offre" << endl << "7 Ajouter des jours de validite" << endl << "8 Changement de Langue" << "9 Recuperer mon Numero" << "10 Mon numero" << "00 Page precedente" << endl << endl;
         
         }else if(choix == 5){
         
             cout << "Votre offre tarifaire actuelle est TOKANA" << endl;
-            cout << "1 MORA (VOIX - SMS- INTERNET)" << endl << "2 FIRST (VOIX - SMS - INTERNET)" << endl << "3 YELLOW (SMS - INTERNET)" << endl << "4 Yas Net (INTERNET)" << endl << "5 ROAMING (DATA - SMS - VOIX)" << "00 Page precedente" << endl << endl
+            cout << "1 MORA (VOIX - SMS- INTERNET)" << endl << "2 FIRST (VOIX - SMS - INTERNET)" << endl << "3 YELLOW (SMS - INTERNET)" << endl << "4 Yas Net (INTERNET)" << endl << "5 ROAMING (DATA - SMS - VOIX)" << "00 Page precedente" << endl << endl;
         
         }else if(choix == 6){
         
             cout << "Produits et Divertissement" << endl;
-            cout << "1 Yas mitsinjo" << endl << "2 MOOZIK" << endl << "3 Zara Soa" << endl << "4 MBALIK" << endl << endl
+            cout << "1 Yas mitsinjo" << endl << "2 MOOZIK" << endl << "3 Zara Soa" << endl << "4 MBALIK" << endl << endl;
             
         }else if(choix == 7){
         
             cout << "Meilleures offres" << endl;
-            cout << "Merci d'avoir choisi Yas. Tapez #322# pour acheter une offre." << endl << endl
+            cout << "Merci d'avoir choisi Yas. Tapez #322# pour acheter une offre." << endl << endl;
         
         }else if(choix == 8){
         
             cout << "Mon identite" << endl;
-        
+            cout << "1 Mon identite enregistre" << endl << endl;
+            
         }else if(choix == 9){
         
-            cout << "onfigurer mon mobile" << endl;
+            cout << "Configurer mon mobile" << endl;
+            cout << "1 (Des-) Activer INTERNET sur compte principal" << endl << endl;
         
         }else if(choix == 00){
         
